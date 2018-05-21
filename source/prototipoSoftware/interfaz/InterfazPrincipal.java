9b75de360ed651e693b513da073a3c76e21d37ba
paola <sandraosorio819@gmail.com>
2018-05-21 12:00:13 -0500

    directorio

--- source/prototipoSoftware/interfaz/InterfazPrincipal.java
+++ source/prototipoSoftware/interfaz/InterfazPrincipal.java
@@ -34,7 +34,7 @@ public class InterfazPrincipal extends JFrame
 	public InterfazPrincipal()
 	{
 		
-		proyecto = new Proyecto("C:\\Users\\Paola\\Documents");
+		proyecto = new Proyecto("C:\\PropuestasGrado");
 		
         setLayout( new BorderLayout( ) );
         setTitle( "Consulta" );
